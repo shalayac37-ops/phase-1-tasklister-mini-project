8cf8c631d0d3a18577507fa5b977b4adf0c2fafd
shalaya <shalayac37@gmail.com>
2026-09-24 22:29:36 -0400

    Complete task lister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const task = event.target.querySelector("#new-task-description").value;
+
+    buildToDo(task);
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    document.getElementById("tasks").appendChild(li);
+  }
 });
